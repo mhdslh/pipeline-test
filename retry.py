b05c4aa2f67043a61ea6b07fdf332b881cdbfce1
Mohammad Salehi <mohammad.salehi@ericsson.com>
2025-04-10 20:45:25 +0000

    v2.0.0

--- retry.py
+++ retry.py
@@ -11,19 +11,12 @@ REF = "main"
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--token")
-    parser.add_argument("--attempt", default="1")
     parser.add_argument("--workflow")
-    parser.add_argument("--dispatch-input")
-
-    # is there any way to add any key value pair dynamically without knowing them in advance?
-
+    parser.add_argument("--attempt", default="1")
+    parser.add_argument("--data")
+    
     args = parser.parse_args()
-
-    print(f"triggered with ${args.attempt}")
-    temp = json.loads(args.dispatch_input)
-    for k, v in temp.items():
-        print(f"====> {k}={v}")
-
+    
     retry = int(args.attempt)
     
     if retry <= 0:
@@ -32,12 +25,9 @@ def main():
     inputs = {
         "attempt": str(retry-1),
     }
-
-    for arg in dynamic_input:
-        if "=" in arg:
-            key, value = arg.split("=", 1)
-            print(f"HERE for {key}={value}")
-            inputs[key.lstrip("-")] = value
+    for k, v in json.loads(args.data).items():
+        inputs[k] = v
+        print(f"{k}={v}")
 
     payload = {
         "ref": REF,
@@ -55,9 +45,6 @@ def main():
     time.sleep(20)
 
     response = requests.post(url, headers=headers, json=payload)
-
-    print("Status Code:", response.status_code)
-    print("Response:", response.text)
 
 
 
